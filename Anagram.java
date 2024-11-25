c0b02c5084089f12f9416eea1cb4325e93a9bbcd
Etay Cohen <etay@altahq.com>
2024-11-25 23:35:44 +0200

    fix: preserving whitespace in anagram

--- Anagram.java
+++ Anagram.java
@@ -70,7 +70,7 @@ public class Anagram {
 
 		for (int i = 0; i < str.length(); i++) {
 			char c = str.charAt(i);
-			if (Character.isAlphabetic(c)) {
+			if (Character.isAlphabetic(c) || Character.isWhitespace(c)) {
 				result += Character.toLowerCase(c);
 			}
 		}
